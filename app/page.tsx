3f973cee8e2de031347fd9e41f1676cb135b9a14
Charldrex <carlos.a41833@gmail.com>
2024-03-08 14:56:28 -0500

    Taller_1

--- app/page.tsx
+++ app/page.tsx
@@ -3,93 +3,36 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+    <div className="flex flex-col gap-7 border-8 border-stone-200	rounded-xl">
+      <div className="flex flex-col gap-7">
+        <h1 className="text-black text-center font-bold text-3xl">C O L O M B IA</h1>
+      </div>
+      <div className="flex flex-raw gap-4 pl-8 ">
+        <img src="https://i.pinimg.com/originals/37/8a/27/378a270e775265622393da8c0527417e.jpg" alt="foto" width={200} height={200} />
+      <div className="flex flex-col gap-4">
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h1 className="font-bold text-xl font-mono	">ANDRES PEDRAZA</h1>
+          <h1 className="font-bold text-xs ">NAME</h1>
         </div>
+        <div>
+          <h1 className="font-bold text-xl font-mono	">COLOMBIANO</h1>
+          <h1 className="font-bold text-xs ">NATIOANALITY</h1>
+        </div>
+        <div>
+          <h1 className="font-bold text-xl font-mono	">23 JUNIO</h1>
+          <h1 className="font-bold text-xs">DATE OF ISSUE</h1>
+        </div>
+        <div>
+          <h1 className="font-bold text-xl font-mono	">COLOMBIA</h1>
+          <h1 className="font-bold text-xs">AUTHORITY</h1>
+        </div>       
+      </div> 
+        
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      
+      
+      
+    </div>
+    
+  )
 }
